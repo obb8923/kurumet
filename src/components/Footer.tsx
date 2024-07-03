962319997fb9314f4d6766a1464a93f5b6f50abc
JeongBin Park <obb8923@naver.com>
2024-07-03 14:57:17 +0900

    FEAT: Footer 생성

---
+++ src/components/Footer.tsx
@@ -0,0 +1,18 @@
+export default function Footer() {
+  return (
+    <div>
+      <hr />
+      <div className="flex justify-center gap-10 py-10 text-gray-500">
+        <p>About</p>
+        <p>Blog</p>
+        <p>Jobs</p>
+        <p>Press</p>
+        <p>Accessibility</p>
+        <p>Partners</p>
+      </div>
+      <p className="mb-10 text-center text-sm text-gray-500">
+        © 2020 Your Company, Inc. All rights reserved.
+      </p>
+    </div>
+  );
+}
