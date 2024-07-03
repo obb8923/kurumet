383a239880365564a7a736877545c59e1d35862c
JeongBin Park <obb8923@naver.com>
2024-07-03 17:16:48 +0900

    FEAT: 음식점json 생성

--- src/components/KakaoMap.tsx
+++ src/components/KakaoMap.tsx
@@ -1,5 +1,5 @@
 import Script from "next/script";
-import { Map } from "react-kakao-maps-sdk";
+import { Map, ZoomControl, MapMarker } from "react-kakao-maps-sdk";
 
 const KAKAO_SDK_URL = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_APP_JS_KEY}&autoload=false`;
 
@@ -10,7 +10,17 @@ export default function KaKaoMap() {
       <Map
         center={{ lat: 37.574187, lng: 126.976882 }} //위도(latitude), 경도(longitude)
         style={{ width: "100%", height: "100%" }}
-      ></Map>
+        level={10}
+      >
+        <ZoomControl position={"RIGHT"} />
+        <MapMarker // 마커를 생성합니다
+          position={{
+            // 마커가 표시될 위치입니다
+            lat: 37.574187,
+            lng: 126.976882,
+          }}
+        />
+      </Map>
     </>
   );
 }
