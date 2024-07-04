d27ffcfc2ca5a635132943f5bbca679502701e33
JeongBin Park <obb8923@naver.com>
2024-07-04 19:55:14 +0900

    CHORE: 주석제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,7 @@ import Map from "../components/KakaoMap";
 
 export default function Home() {
   return (
-    // <main className="absolute w-full h-full left-0 top-0">
     <main className="flex justify-center">
-      {/* <div className="w-96 h-96 bg-black"></div>
-      <div className="w-96 h-96 bg-white"></div> */}
       <div className="w-96 h-96 m-10" style={{ width: "90vw", height: "90vh" }}>
         <Map />
       </div>
