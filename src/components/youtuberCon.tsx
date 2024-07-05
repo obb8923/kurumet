2dc17ac9e21405c1490e973539f66ce4f7635a48
JeongBin Park <obb8923@naver.com>
2024-07-05 19:14:50 +0900

    FEAT: 유튜버콘 전역상태변경 기능 추가

--- src/components/youtuberCon.tsx
+++ src/components/youtuberCon.tsx
@@ -1,7 +1,19 @@
-export default function YoutuberCon(props: string) {
+import { useStore } from "@/store/StateCon";
+type props = {
+  name: string;
+};
+export default function YoutuberCon(props: props) {
+  const { pushList, popList, findList } = useStore();
+  const clickHandler = (name: string) => {
+    findList(name) ? popList(name) : pushList(name);
+    console.log("list:", useStore.getState().list);
+  };
   return (
-    <div className="rounded-full">
-      <></>
+    <div
+      className="rounded-full bg-blue-300 hover:cursor-pointer"
+      onClick={() => clickHandler(props.name)}
+    >
+      <p>{props.name}</p>
     </div>
   );
 }
