a8ed636eb585bf8ef936ccdc8b5dc9f4b439891d
JeongBin Park <obb8923@naver.com>
2024-07-05 22:17:01 +0900

    REFACTOR: atomic export

--- src/store/StateCon.tsx
+++ src/store/StateCon.tsx
@@ -3,26 +3,32 @@ type storeState = {
   list: Set<string>;
 };
 type action = {
-  pushList: (item: string) => void;
-  findList: (item: string) => boolean;
-  popList: (item: string) => void;
+  actions: {
+    pushList: (item: string) => void;
+    findList: (item: string) => boolean;
+    popList: (item: string) => void;
+  };
 };
-export const useStore = create<storeState & action>((set) => ({
+const useStore = create<storeState & action>((set, get) => ({
   list: new Set(),
-  pushList: (item) =>
-    set((state) => {
-      const tmpSet = new Set(state.list);
-      tmpSet.add(item);
-      return { list: tmpSet };
-    }),
-  findList: (item) => {
-    const tmpState: storeState = useStore.getState();
-    return tmpState.list.has(item);
+  actions: {
+    pushList: (item) =>
+      set((state) => {
+        const tmpSet = new Set(state.list);
+        tmpSet.add(item);
+        return { list: tmpSet };
+      }),
+    findList: (item) => {
+      return get().list.has(item);
+    },
+    popList: (item) =>
+      set((state) => {
+        const tmpSet = new Set(state.list);
+        tmpSet.delete(item);
+        return { list: tmpSet };
+      }),
   },
-  popList: (item) =>
-    set((state) => {
-      const tmpSet = new Set(state.list);
-      tmpSet.delete(item);
-      return { list: tmpSet };
-    }),
 }));
+
+export const useList = () => useStore((state) => state.list);
+export const useActions = () => useStore((state) => state.actions);
