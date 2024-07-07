02b09732c160b9ba2c093d8b28d80565e37c213e
JeongBin Park <obb8923@naver.com>
2024-07-07 18:33:38 +0900

    FEAT: GNB에 Search추가

--- src/components/GNB.tsx
+++ src/components/GNB.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import Image from "next/image";
 import userProfile from "../../public/userProfile.svg";
 import YoutuberCon from "./youtuberCon";
+import Search from "./Search";
 export default function GNB() {
   const [isLogin, setIsLogin] = useState(false);
 
@@ -25,6 +26,10 @@ export default function GNB() {
       <div className="flex gap-10 flex-1 my-1 py-1 px-8">
         <YoutuberCon name="공슐랭 가이드"></YoutuberCon>
         <YoutuberCon name="먹을텐데"></YoutuberCon>
+      </div>
+      {/* search Section */}
+      <div className=" py-1 px-8">
+        <Search />
       </div>
       {/* login Section */}
       <div className="bg-red-600 my-1 py-1 px-8 flex justify-end">
