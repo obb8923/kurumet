8a67238a0dcaa2111d76b34526e946018417a963
JeongBin Park <obb8923@naver.com>
2024-07-07 22:34:54 +0900

    FEAT: Dropdown으로 지도,리스트 전환 가능하도록 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
+"use client";
 import Image from "next/image";
 import Map from "../components/KakaoMap";
-
+import Dropdown from "@/components/Dropdown";
+import { useMapState } from "@/store/StateMap";
+import List from "@/components/List";
 export default function Home() {
+  const mapState = useMapState();
   return (
-    <main className="flex justify-center">
-      <div className="w-96 h-96 m-10" style={{ width: "90vw", height: "90vh" }}>
-        <Map />
+    <main className="flex flex-col justify-center items-center">
+      <div className="m-2 flex justify-end" style={{ width: "90vw" }}>
+        <Dropdown />
+      </div>
+
+      <div className="w-96 h-96 " style={{ width: "90vw", height: "90vh" }}>
+        {mapState === "지도" && <Map />}
+        {mapState === "리스트" && <List />}
       </div>
     </main>
   );
