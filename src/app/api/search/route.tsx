769e853d49ea10501719bc0a1d05c10ba8518c39
JeongBin Park <obb8923@naver.com>
2024-07-07 21:18:01 +0900

    FEAT: Search API 추가

---
+++ src/app/api/search/route.tsx
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import programs from "@/../public/list/programs.json";
+
+const removeSpaces = (v: string): string => {
+  return v.replace(/\s+/g, "");
+};
+
+export async function POST(request: Request) {
+  const { search } = await request.json();
+
+  const foundProgram = programs.programs.find((v) => {
+    return removeSpaces(v) === removeSpaces(search);
+  });
+
+  if (foundProgram) {
+    return NextResponse.json(
+      { message: "found", program: foundProgram },
+      { status: 200 }
+    );
+  } else {
+    return NextResponse.json({ message: "not found" }, { status: 200 });
+  }
+}
