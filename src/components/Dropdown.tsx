a043554b8aa8591b24cf33ec5a10eccd8edd029d
JeongBin Park <obb8923@naver.com>
2024-07-08 22:50:56 +0900

    REFACTOR: svg 표기방식변경(스네이크-카멜)

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -16,7 +16,7 @@ export default function Dropdown() {
       <button
         id="dropdownDefaultButton"
         data-dropdown-toggle="dropdown"
-        className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        className="textWhite bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         type="button"
         onClick={() => {
           toggleDropdown("");
@@ -32,9 +32,9 @@ export default function Dropdown() {
         >
           <path
             stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
             d="m1 1 4 4 4-4"
           />
         </svg>
@@ -43,14 +43,14 @@ export default function Dropdown() {
       {isOpen && (
         <div
           id="dropdown"
-          className="absolute top-24 z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
+          className="absolute top-24 z-10 bgWhite divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
         >
           <ul
             className="py-2 text-sm text-gray-700 dark:text-gray-200"
             aria-labelledby="dropdownDefaultButton"
           >
             <li
-              className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+              className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:textWhite"
               onClick={() => {
                 toggleDropdown("지도");
               }}
@@ -58,7 +58,7 @@ export default function Dropdown() {
               지도
             </li>
             <li
-              className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+              className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:textWhite"
               onClick={() => {
                 toggleDropdown("리스트");
               }}
