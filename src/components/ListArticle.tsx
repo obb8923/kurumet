5edc1dffe86fcf9cd87cd5c9f79d31bc55d513e6
JeongBin Park <obb8923@naver.com>
2024-07-09 22:57:52 +0900

    FEAT: 무한스크롤 내용

--- src/components/ListArticle.tsx
+++ src/components/ListArticle.tsx
@@ -2,10 +2,18 @@ type propsType = {
   food: string;
   youtubeEmbedd: string;
 };
-export default function ListArticle(props: propsType) {
+
+export default function ListArticle({ food, youtubeEmbedd }: propsType) {
   return (
-    <>
-      <></>
-    </>
+    <article className="flex flex-col">
+      <iframe
+        src={youtubeEmbedd}
+        frameBorder="0"
+        allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      />
+      <p>{food}</p>
+    </article>
   );
 }
