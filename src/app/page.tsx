2ad6128948878d9c132c4499c2cdd778c1ac6929
JeongBin Park <obb8923@naver.com>
2024-07-11 07:13:36 +0900

    STYLE: 맵과 리스트의 배치 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,11 +11,16 @@ export default function Home() {
       <div className="m-2 flex justify-end" style={{ width: "90vw" }}>
         <Dropdown />
       </div>
-
-      <div className="w-96 h-96 " style={{ width: "90vw", height: "90vh" }}>
-        {mapState === "지도" && <Map />}
-        {mapState === "리스트" && <List />}
-      </div>
+      {mapState === "지도" && (
+        <div className="w-96 h-96 " style={{ width: "90vw", height: "90vh" }}>
+          <Map />
+        </div>
+      )}
+      {mapState === "리스트" && (
+        <div style={{ maxWidth: "90vw", maxHeight: "90vh" }}>
+          <List />
+        </div>
+      )}
     </main>
   );
 }
