fd0139f11738eec33d9130848ecdfb0817c68ae3
JeongBin Park <obb8923@naver.com>
2024-07-11 21:19:31 +0900

    FEAT: 전역상태 변경시, 리스트 보이고, 안보이게 하는 기능 추가

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,34 +1,78 @@
 import { useState, useEffect } from "react";
 import ListArticle from "./ListArticle";
 import { useInView } from "react-intersection-observer";
-
+import { useList, useActions } from "@/store/StateCon";
+type listType = {
+  name: string;
+  food: string;
+  stars: number;
+  address: string;
+  latlng: { lat: number; lng: number };
+  youtube: string;
+  youtubeEmbed: string;
+};
 export default function List() {
+  const l = useList();
+  const [items, setItems] = useState<listType[]>([]);
   const { ref, inView } = useInView({
     threshold: 0,
   });
   const [fragment, setFragment] = useState<React.ReactNode[]>([]);
   const [key, setKey] = useState<number>(0);
+
   useEffect(() => {
-    if (inView && key < 100) {
-      console.log(key, inView);
-      setKey((prev) => prev + 1);
+    async function getincon() {
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ list: Array.from(l) }),
+      };
+      const res = await fetch("/api/inCon", options);
+      const data = await res.json();
+      setItems((prev) => data.listArray);
+    }
+    getincon();
+  }, [l, ref]);
+
+  useEffect(() => {
+    if (inView && items.length > 0 && items[key]) {
       const newF = (
         <ListArticle
-          key={key}
-          food="된찌"
-          youtubeEmbedd="https://www.youtube.com/embed/Tzyt91TYjLA?si=WXAhPcXBp9CN5Anf"
+          key={`${key} - ${items[key].name}-${items[key].latlng}`}
+          food={items[key].food}
+          youtubeEmbedd={items[key].youtubeEmbed}
         ></ListArticle>
       );
+      setKey((prev) => prev + 1);
       setFragment((prev) => [...prev, newF]);
     }
-  }, [inView, fragment]);
+  }, [inView, fragment, items, key]);
+
+  useEffect(() => {
+    setFragment([]);
+  }, [items]);
 
   return (
-    <section className="bg-gray-200 h-full w-full">
-      <div className="h-full w-full flex flex-wrap justify-evenly">
-        {fragment}
-      </div>
-      <div ref={ref} className="h-10"></div>
+    <section className="bg-gray-200 h-full w-full flex flex-col">
+      {items.length == 0 && (
+        <div
+          className="w-96 h-96 flex justify-center items-center"
+          style={{ width: "90vw", height: "90vh" }}
+        >
+          <small>선택된 프로그램이 없습니다.</small>
+        </div>
+      )}
+
+      {items.length > 0 && (
+        <>
+          <div className="h-full w-full flex flex-wrap gap-8 justify-evenly">
+            {fragment}
+          </div>
+          <div ref={ref} className="h-1"></div>
+        </>
+      )}
     </section>
   );
 }
