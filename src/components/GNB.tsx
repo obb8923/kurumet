38e0de50c7810249bca0620ab8705cd8b2979f9d
JeongBin Park <obb8923@naver.com>
2024-07-12 14:52:54 +0900

    STYLE: GNB flex정렬, m,b 비율 변경

--- src/components/GNB.tsx
+++ src/components/GNB.tsx
@@ -1,7 +1,5 @@
 "use client";
 import Link from "next/link";
-import Image from "next/image";
-import userProfile from "../../public/userProfile.svg";
 import YoutuberCon from "./youtuberCon";
 import Search from "./Search";
 import { useList } from "@/store/StateCon";
@@ -14,21 +12,28 @@ export default function GNB() {
   return (
     <nav className="flex shadow">
       {/* logo Section */}
-      <div className="my-1 py-1 px-8 border-r border-black border-solid">
-        <Link href="/">logo</Link>
+      <div className="flex justify-center items-center my-1 py-1 px-8 border-r border-black border-solid hover:cursor-pointer">
+        <Link href="/" className="logo">
+          kurumet
+        </Link>
       </div>
       {/* youtuberCon Section */}
-      <div className="flex gap-10 flex-1 my-1 py-1 px-8">
+      <div className="flex gap-4 flex-1 my-2 px-8">
         <YoutuberCon name="공슐랭 가이드"></YoutuberCon>
         <YoutuberCon name="먹을텐데"></YoutuberCon>
       </div>
       {/* search Section */}
-      <div className=" py-1 px-8">
+      <div className="flex justify-center items-center mx-4">
         <Search />
       </div>
       {/* login Section */}
-      <div className="bg-red-600 my-1 py-1 px-8 flex justify-end">
-        <Image src={userProfile} alt="userProfile"></Image>
+      <div className=" flex justify-center items-center mx-4">
+        <button
+          type="button"
+          className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
+        >
+          로그인
+        </button>
       </div>
     </nav>
   );
