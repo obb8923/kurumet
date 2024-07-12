815e06b60be83e2aeacb0a1536f45c38c7205e49
JeongBin Park <obb8923@naver.com>
2024-07-12 14:50:27 +0900

    STYLE :  Search 스타일 변경(하양 회색으로 통일)

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -146,7 +146,7 @@ export default function Search() {
             <button
               type="submit"
               disabled={isSubmitting && togleSelected === "카테고리"}
-              className="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              className="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-white rounded-e-lg border border-gray-300 hover:bg-gray-100 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               <svg
                 className="w-4 h-4"
@@ -156,7 +156,7 @@ export default function Search() {
                 viewBox="0 0 20 20"
               >
                 <path
-                  stroke="currentColor"
+                  stroke="black"
                   strokeLinecap="round"
                   strokeLinejoin="round"
                   strokeWidth="2"
