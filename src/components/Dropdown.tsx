92d324547fe4b7c36057c11c45d8ca28322176a7
JeongBin Park <obb8923@naver.com>
2024-07-12 14:48:56 +0900

    STYLE: 드롭다운 스타일 변경(하양 회색으로 통일)

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -16,7 +16,7 @@ export default function Dropdown() {
       <button
         id="dropdownDefaultButton"
         data-dropdown-toggle="dropdown"
-        className="textWhite bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        className="textWhite bg-white hover:bg-gray-100 border border-gray-300 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         type="button"
         onClick={() => {
           toggleDropdown("");
