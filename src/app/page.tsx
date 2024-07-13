a55b2e3aec7327c0b6a4d28aac5af418c5a6ac42
JeongBin Park <obb8923@naver.com>
2024-07-13 18:45:45 +0900

    FEAT: 유튭콘 선택한게 없을때 지도에 유도 문구 나오는 기능 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@ import Map from "../components/KakaoMap";
 import Dropdown from "@/components/Dropdown";
 import { useMapState } from "@/store/StateMap";
 import List from "@/components/List";
+import { useList } from "@/store/StateCon";
 export default function Home() {
+  const l = useList();
   const mapState = useMapState();
   return (
     <main className="flex flex-col items-center">
@@ -11,8 +13,18 @@ export default function Home() {
         <Dropdown />
       </div>
       {mapState === "지도" && (
-        <div className="w-96 h-96 " style={{ width: "90vw", height: "90vh" }}>
-          <Map />
+        <div
+          className="relative w-96 h-96"
+          style={{ width: "90vw", height: "90vh" }}
+        >
+          {l.size == 0 && (
+            <div className="absolute w-full h-full z-10 bg-gray-200  flex justify-center items-center">
+              <small>보고싶은 유튜버를 클릭해보세요</small>
+            </div>
+          )}
+          <div className="absolute w-full h-full z-0">
+            <Map />
+          </div>
         </div>
       )}
       {mapState === "리스트" && (
