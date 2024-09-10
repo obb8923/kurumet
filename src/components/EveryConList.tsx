6ea4f92ba820c1a1a5839ed2b8cdbd209f093beb
JeongBin Park <obb8923@naver.com>
2024-09-10 11:30:04 +0900

    FEAT: 모든 유튭콘을 보여주는 리스트 생성

---
+++ src/components/EveryConList.tsx
@@ -0,0 +1,10 @@
+import YoutuberCon from "./youtuberCon";
+const EveryConList = () => {
+  return (
+    <div className="w-full h-full flex gap-2">
+      <YoutuberCon name="공슐랭 가이드" />
+      <YoutuberCon name="먹을텐데" />
+    </div>
+  );
+};
+export default EveryConList;
