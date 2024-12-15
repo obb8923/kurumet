917ec5c7a7915570b8dc5e4bb70e61eafe0089c0
Jeongbin Park <obb8923@naver.com>
2024-12-15 21:14:38 +0900

    fix/localStorage is not defined

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,17 +1,24 @@
 "use client"
 import Image from "next/image";
+import { useEffect, useState } from "react";
+
 const LoginButton = () => {
-  const id = localStorage.getItem("id");
-  const image = localStorage.getItem("profile_image");
+  const [id, setId] = useState<string | null>(null);
+  const [image, setImage] = useState<string | null>(null);
+
+  useEffect(() => {
+    setId(localStorage.getItem("id"));
+    setImage(localStorage.getItem("profile_image"));
+  }, []);
+
   const handleLogin = () => {
     if(id) {
-
-    }else{
+      // 로그인된 상태에서의 동작
+    } else {
       // 카카오 로그인 페이지로 리다이렉션
       const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_KAKAO_RESTAPI_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}`;
       window.location.href = KAKAO_AUTH_URL;
     }
-  
   };
 
   return (id ? <div onClick={handleLogin} className="hover:cursor-pointer">
