6e75f9473806c98bd48b757ddb767972bc331ff2
Jeongbin Park <obb8923@naver.com>
2024-12-15 21:11:30 +0900

    로그인시 버튼 변경, 이미지 추가

--- src/components/LoginButton.tsx
+++ src/components/LoginButton.tsx
@@ -1,11 +1,23 @@
+import Image from "next/image";
 const LoginButton = () => {
+  const id = localStorage.getItem("id");
+  const image = localStorage.getItem("profile_image");
   const handleLogin = () => {
-    const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_KAKAO_RESTAPI_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}`;
-    // 카카오 로그인 페이지로 리다이렉션
-    window.location.href = KAKAO_AUTH_URL;
+    if(id) {
+
+    }else{
+      // 카카오 로그인 페이지로 리다이렉션
+      const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_KAKAO_RESTAPI_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}`;
+      window.location.href = KAKAO_AUTH_URL;
+    }
+  
   };
 
-  return <button onClick={handleLogin}>카카오로 로그인</button>;
+  return (id ? <div onClick={handleLogin} className="hover:cursor-pointer">
+    <Image src={image || ''} alt="profile" 
+    className="rounded-full w-8 h-8 bg-gray-300 object-cover"
+    width={32} height={32} />
+  </div> : <button onClick={handleLogin}>카카오로 로그인</button>);
 };
 
 export default LoginButton;
