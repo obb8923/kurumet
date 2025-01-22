1e97c463e7db908f9d0ec7639d254dc8819bc5b0
Jeongbin Park <obb8923@naver.com>
2025-01-23 01:21:18 +0900

    feat/ 커스텀 animated-ping 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      animation: {
+        'ping-slow': 'ping 2s cubic-bezier(0, 0, 0.2, 1) infinite',  // 더 느리고 작게
+      },
     },
   },
   plugins: [],
