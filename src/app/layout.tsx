87396e879d4a72a33c10bf4613c5c618bc61236e
Jeongbin Park <obb8923@naver.com>
2025-01-23 16:22:49 +0900

    feat/ GA4 첫추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 // import GNB from "../components/GNB";
 import GNB2 from "../components/GNB2";
 import Footer from "../components/Footer";
+import Script from 'next/script';
 
 export const viewport = {
   width: 'device-width',
@@ -26,6 +27,17 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
+    <>
+    {/* <!-- Google tag (gtag.js) --> */}
+    <Script src="https://www.googletagmanager.com/gtag/js?id=G-QKX95L8C6C" />
+    <Script id="google-analytics">
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-QKX95L8C6C');
+      `}
+    </Script>
     <html>
       <body className="min-h-screen flex flex-col justify-between">
         {/* 검색 기능과 로그인 기능 구현 전 까지는 GNB2만 보이게 함 */}
@@ -35,5 +47,6 @@ export default function RootLayout({
         <Footer />
       </body>
     </html>
+    </>
   );
 }
